98e3b406ff54ad68c0536e1a186dd84770fd5b0a
abgese <abraham.g.sebastian@gmail.com>
2015-10-23 18:43:55 +0530

    Added Spectral Clustering code

--- spectral.py
+++ spectral.py
@@ -2,13 +2,16 @@ import numpy as np
 import pandas as pd
 import scipy as sp
 from scipy.stats import pearsonr
+from numpy.linalg import eig
 
 def read_data():
 	data = pd.read_csv("u.tsv",delimiter='\t')
 	return data
 
 def RatingMatrix( data ):
-	R = np.zeros((943,1682))
+	m = len(data.groupby('User').aggregate('count'))
+	n = len(data.groupby('Movie').aggregate('count'))
+	R = np.zeros((m,n))
 	for i in data.index:
 		u = data.User[i]
 		m = data.Movie[i]
@@ -17,11 +20,24 @@ def RatingMatrix( data ):
 	return R
 
 def SimilarityMatrix( R ):
-	S = np.zeros((943,943))
-	for i in range(943):
-		for j in range(i+1,943):
+	m = len(R)
+	S = np.zeros((m,m))
+	for i in range(m):
+		for j in range(i+1,m):
        			 S[i][j],_ = pearsonr(R[i],R[j])
        			 S[j][i] = S[i][j]
 	return S
+
+def Spectral( S , V ):
+    values,vectors = eig(S)
+    vect = vectors[:,values.argsort()[1]]
+    V1=[]
+    V2=[]
+    for i in range(len(vect)):
+        if(vect[i] < 0):
+            V1.append(V[i])
+        else:
+            V2.append(V[i])
+    return V1,V2
 
 
